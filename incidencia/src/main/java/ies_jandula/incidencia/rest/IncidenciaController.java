5d51db2d624a5230f71f5633b9e856d4b925a83e
JasonDGian <jasdavg@gmail.com>
2024-10-08 16:18:33 +0200

    Endpoint update. Adds comments to modified endpoints explaining behavior.
    Comenced the substitution of String type returns for ResponseEntity objects.

--- incidencia/src/main/java/ies_jandula/incidencia/rest/IncidenciaController.java
+++ incidencia/src/main/java/ies_jandula/incidencia/rest/IncidenciaController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ies_jandula.incidencia.models.Incidencia;
 import ies_jandula.incidencia.repository.IIncidenciaRepository;
 import ies_jandula.incidencia.utils.Constants;
-// import ies_jandula.incidencia.utils.EstadoIncidencia; DESACTIVADO A FAVOR DE TIPO STRING
 
 @RestController
 @RequestMapping(value = "/incidencias")
@@ -27,23 +28,49 @@ public class IncidenciaController
 	@Autowired
 	IIncidenciaRepository repo;
 
-	// Adaptar toda la logica del controlador al uso de este repositorio.
-	// Gestionar las excepciones y validaciones desde el controlador.
-
-	// LISTAR TODASS INCIDENCIAS.
+	/**
+	 * Atiende a peticiones GETR en localhost:8888/incidencias 
+	 * 
+	 * Si la operación es exitosa, devuelve una respuesta con un estado HTTP 200 OK
+	 * y un cuerpo que contiene una lista de objetos de tipo `Incidencia`.
+	 * 
+	 * En caso de que ocurra un error durante la recuperación de las incidencias, se
+	 * captura la excepción y se devuelve una respuesta con un estado HTTP 500
+	 * Internal Server Error, sin contenido en el cuerpo de la respuesta.
+	 * 
+	 * @return ResponseEntity<List<Incidencia>> - Un objeto ResponseEntity 
+	 *         que contiene la lista de incidencias y el estado de la respuesta.
+	 */
 	@GetMapping // <localhost>/incidencias GET
-	public List<Incidencia> listarIncidencias()
+	public ResponseEntity<List<Incidencia>> listarIncidencias()
 	{
-		return repo.findAll();
+		try
+		{
+			return ResponseEntity.ok().body(repo.findAll());
+		} catch (Exception e)
+		{
+			return ResponseEntity.internalServerError().build();
+		}
 	}
 
 	@PostMapping // <localhost>:8888/incidencias POST
-	public Optional<Incidencia> buscaIncidencia(@RequestParam long id)
+	public ResponseEntity<?> buscaIncidencia(@RequestParam long id)
 	{
-		// Metodo que devuelve un optional porque no sabemos si el ID buscado
-		// existe ne la bbdd o no.
+		try
+		{
+			Optional<Incidencia> optIncidencia = repo.findById(id);
 
-		return repo.findById(id);
+			if (optIncidencia.isPresent())
+			{
+				return ResponseEntity.ok().body(optIncidencia.get());
+			} else
+			{
+				return ResponseEntity.badRequest().body("No existen incidencias con esta ID");
+			}
+		} catch (Exception e)
+		{
+			return ResponseEntity.internalServerError().body("Se ha producido un error");
+		}
 	}
 
 	// Generar una nueva incidencia.
@@ -74,24 +101,24 @@ public class IncidenciaController
 
 		return "Incidencia creada " + incidencia.toString();
 	}
-	
+
 	// Cambiar a RESUELTA la incidencia con el ID proporcionado.
 	@PostMapping(value = "/reinicia") // <localhost>/incidencias/resuelve POST
-	public String reiniciaIncidencia(@RequestParam(required = true) long id)
+	public ResponseEntity<String> reiniciaIncidencia(@RequestParam(required = true) long id)
 	{
 		return modificaEstadoIncidencia(id, Constants.PENDIENTE);
 	}
 
 	// Cambiar a RESUELTA la incidencia con el ID proporcionado.
 	@PostMapping(value = "/resuelve") // <localhost>/incidencias/resuelve POST
-	public String resuelveIncidencia(@RequestParam(required = true) long id)
+	public ResponseEntity<String> resuelveIncidencia(@RequestParam(required = true) long id)
 	{
 		return modificaEstadoIncidencia(id, Constants.RESUELTA);
 	}
 
 	// Cambiar a CANCELADA la incidencia con el ID proporcionado.
 	@PostMapping(value = "/cancelar") // <localhost>/incidencias/cancela POST
-	public String cancelaIncidencia(@RequestParam(required = true) long id)
+	public ResponseEntity<String> cancelaIncidencia(@RequestParam(required = true) long id)
 	{
 
 		return modificaEstadoIncidencia(id, Constants.CANCELADA);
@@ -99,7 +126,7 @@ public class IncidenciaController
 
 	// Cambiar a CANCELADA la incidencia con el ID proporcionado.
 	@PostMapping(value = "/en-progreso") // <localhost>/incidencias/cancela POST
-	public String activaIncidencia(@RequestParam(required = true) long id)
+	public ResponseEntity<String> activaIncidencia(@RequestParam(required = true) long id)
 	{
 
 		return modificaEstadoIncidencia(id, Constants.EN_PROGRESO);
@@ -114,37 +141,57 @@ public class IncidenciaController
 	}
 
 	/**
-	 * Metodo que recibe un id de una incidencia y un estado para aplicarle. Si no
-	 * existen incidencias con ese ID devuelve un mensaje (String) que informa. Si
-	 * la incidencia ya está en ese estado devuelve un aviso (String) y si la
-	 * operacion se lleva a cabo con éxito devuelve un mensaje de informe.
+	 * Este método permite modificar el estado de una incidencia identificada por su
+	 * ID. Realiza las siguientes validaciones y acciones:
 	 * 
-	 * @param id
-	 * @param estado
-	 * @return
+	 * 1. Busca la incidencia en el repositorio usando el ID proporcionado. - Si no
+	 * se encuentra una incidencia con ese ID, devuelve un mensaje de error
+	 * informando que no existe.
+	 * 
+	 * 2. Si se encuentra la incidencia: - Verifica si ya tiene el estado que se
+	 * desea aplicar. - Si ya está en el mismo estado, devuelve un mensaje de aviso
+	 * indicando que no es necesario actualizarla. - Si la incidencia tiene un
+	 * estado diferente: - Actualiza el estado de la incidencia con el nuevo valor.
+	 * - Guarda los cambios en el repositorio. - (Posible envío de aviso al
+	 * profesor, pero este código no está implementado aquí). - Devuelve un mensaje
+	 * de éxito indicando que el estado ha sido modificado correctamente.
+	 * 
+	 * @param id     - El identificador único de la incidencia a modificar.
+	 * @param estado - El nuevo estado que se desea aplicar a la incidencia.
+	 * @return Un mensaje indicando el resultado de la operación: - "ERROR: No
+	 *         existen incidencias con ese ID." si la incidencia no se encuentra. -
+	 *         "AVISO: La incidencia ya estaba configurada como [estado]." si el
+	 *         estado ya es el solicitado. - "EXITO: Incidencia modificada a
+	 *         [estado] con éxito." si la actualización fue exitosa.
 	 */
-	private String modificaEstadoIncidencia(long id, String estado)
+	private ResponseEntity<String> modificaEstadoIncidencia(long id, String estado)
 	{
-
-		Optional<Incidencia> optIncidencia = repo.findById(id);
-
-		if (optIncidencia.isPresent())
+		try
 		{
-			Incidencia incidencia = optIncidencia.get();
+			Optional<Incidencia> optIncidencia = repo.findById(id);
 
-			if (incidencia.getEstadoIncidencia().equals(estado))
+			if (optIncidencia.isPresent())
 			{
-				return "AVISO: La incidencia ya estaba configurada como " + estado + ".";
+				Incidencia incidencia = optIncidencia.get();
+
+				if (incidencia.getEstadoIncidencia().equals(estado))
+				{
+					return ResponseEntity.ok().body("AVISO: La incidencia ya esta configurada como " + estado + ".");
+				} else
+				{
+					incidencia.setEstadoIncidencia(estado);
+					repo.saveAndFlush(incidencia);
+					// llamar a metodo de aviso a profesor (mandar correo de aviso).
+					return ResponseEntity.ok().body("EXITO: Incidencia modificada a " + estado + " con éxito.");
+				}
 			} else
 			{
-				incidencia.setEstadoIncidencia(estado);
-				repo.saveAndFlush(incidencia);
-				// llamar a metodo de aviso a profesor (mandar correo de aviso).
-				return "Incidencia modificada a " + estado + " con éxito.";
+				return ResponseEntity.badRequest().body("ERROR: No existen incidencias con ese ID.");
 			}
-		} else
+		} catch (Exception e)
 		{
-			return "ERROR: No existen incidencias con ese ID.";
+
+			return ResponseEntity.internalServerError().body("Se ha producido un error.");
 		}
 	}
 }
